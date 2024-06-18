c38ff55e7fd9f6ffa6005f23b123c4d20b986ef5
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-06-18 12:04:06 -0300

    conversor de moedas

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const convertButton = document.querySelector(".convert-button")
+
+function convertValues(){
+   const inputCurrencyValue = document.querySelector(".input-currency").value
+   const currencyValueToConvert = document.querySelector(".currency-value-to-convert")// Valor em Real
+   const currencyValueConverted = document.querySelector(".currency-value")// Outras moedas
+
+   const dolarToday = 5.2
+
+   const convertedValue = inputCurrencyValue / dolarToday
+
+   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+   }).format(inputCurrencyValue)
+
+
+   currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD"
+   }).format(convertedValue)
+
+}
+
+convertButton.addEventListener("click", convertValues)
