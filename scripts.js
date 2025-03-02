6b41e43d34863bfa1c61dab389b8ef1036341fb8
Carlos <fariascarlos2346@gmail.com>
2025-03-02 16:16:26 -0300

    concluido e indo para o desafio

--- scripts.js
+++ scripts.js
@@ -1,6 +1,65 @@
 const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
 
-function convertValues(){
+function convertValues() {
+   const inputCurrencyValue = document.querySelector(".input-currency").value
+   const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+   const currencyValueConverted = document.querySelector(".currency-value")
+
+   const dolarToday = 5.2
+   const euroToday = 6.2
+
+   if(currencySelect.value == "dolar") {
+      // Se o Select estiver selecionado o valor de dolar, entre aqui
+      currencyValueConverted.innerHTML = new Intl.NumberFormat("en-US", {
+         style: "currency",
+         currency: "USD"
+      }).format(inputCurrencyValue / dolarToday)
+   }
+
+   if(currencySelect.value == "euro") {
+      // se o select estiver selecionado o valor de euro, entre aqui
+      currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE",{
+         style: "currency",
+         currency: "EUR"
+      }).format(inputCurrencyValue / euroToday)
+   }
+
+   currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+   }).format(inputCurrencyValue)
+}
+
+function changeCurrency() {
+   const currencyName = document.getElementById("currency-name")
+   const currencyImage = document.querySelector(".currency-img")
+
+   if (currencySelect.value == "dolar") {
+      currencyName.innerHTML = "Dólar americano"
+      currencyImage.src = "./assets/dolar.png"
+   }
+
+   if (currencySelect.value == "euro") {
+      currencyName.innerHTML = "Euro"
+      currencyImage.src = "./assets/euro.png"
+   }
+
+   convertValues()
+}
+
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
+
+
+
+
+
+
+
+//const convertButton = document.querySelector(".convert-button")
+
+/*function convertValue(){
    const inputCurrencyValue = document.querySelector(".input-currency").value
    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
    const currencyValueConvert = document.querySelector(".currency-value")
@@ -14,4 +73,4 @@ function convertValues(){
    console.log(convertedValue)
 }
 
-convertButton.addEventListener("click", convertValues)
+convertButton.addEventListener("click", convertValues)*/
